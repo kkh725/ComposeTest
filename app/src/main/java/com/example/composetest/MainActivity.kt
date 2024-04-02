8ea3d385b67a50a22b63c76bf794865559b07e13
kkh <kkhyungyung0@naver.com>
2024-04-02 16:59:22 +0900

    image

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -27,6 +28,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -83,6 +85,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         MyButton(text = "text") {
             
         }
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "description")
         Text(
 
             text = text,
