07ad24e80acf6e4b7745dab3b0113ccd82ecd7a5
kkh <kkhyungyung0@naver.com>
2024-04-02 15:52:23 +0900

    Row

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composetest.ui.theme.ComposeTestTheme
@@ -34,20 +42,39 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var text by remember {
+        mutableStateOf(name)
+    }
     Column {
+        Row {
+            Text(text = "hi ")
+            Text(text = "hi ")
+            Text(text = "hi")
+        }
         Text(
-            text = "Hello $name!",
+            text = text,
             modifier = modifier
         )
         Text(text = "Text")
+        Button(onClick = { text = "hello" }) {
+            Text(text = text)
+        }
     }
 
+}
+@Composable
+fun GreetingScreen() {
+    Scaffold(
+        // Scaffold 설정
+    ) {
+        Greeting(name = "Your Name")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() { //매게변수를 사용하지않아야한다.
     ComposeTestTheme {
-        Greeting("Android")
+        GreetingScreen()
     }
 }
