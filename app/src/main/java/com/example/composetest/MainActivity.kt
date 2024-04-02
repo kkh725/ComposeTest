86f9fb72096fa34e6c2c51f9d3fa39b1540d614e
kkh <kkhyungyung0@naver.com>
2024-04-02 16:15:09 +0900

    Padding

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -21,7 +23,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composetest.ui.theme.ComposeTestTheme
 
 class MainActivity : ComponentActivity() {
@@ -40,16 +47,18 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+//컴포저블 함수 자체가 그냥 하나의 view혹은 레이아웃이라고 생각하면된다.
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var text by remember {
         mutableStateOf(name)
     }
     Column {
+        modifier.padding(16.dp)
         Row {
-            Text(text = "hi ")
-            Text(text = "hi ")
-            Text(text = "hi")
+            Text(text = "hi ", color = Color.Red, fontSize = 25.sp)
+            Text(text = "hi ", textAlign = TextAlign.Center, fontWeight = FontWeight.Bold)
+            Text(text = "hi",modifier.padding(start = 10.dp, top = 45.dp))
         }
         Text(
             text = text,
