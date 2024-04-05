9e66cbdb9d7496f64865ba777bd91115430784c0
kkh <kkhyungyung0@naver.com>
2024-04-06 01:20:39 +0900

    scaffold

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
@@ -24,9 +25,11 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
@@ -36,6 +39,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.composetest.ui.theme.ComposeTestTheme
 import androidx.navigation.compose.rememberNavController
@@ -171,10 +175,15 @@ fun GreetingPreview() {
         Scaffold(
             bottomBar = {
                 BottomAppBar {
-                    Text(text = "hi")
+                    Text(text = "hidddd", color = Color.Red, fontSize = 35.sp)
+                    Spacer(modifier = Modifier.width(10.dp))
+                    Button(onClick = { /*TODO*/ }) {
+
+                        
+                    }
                 }
             },
-            
+
             floatingActionButton = {
                 FloatingActionButton(
                     onClick = {
