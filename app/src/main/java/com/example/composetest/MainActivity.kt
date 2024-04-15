c155c7b88ebcbb6ebeed6bb9838268c91daae151
kkh <kkhyungyung0@naver.com>
2024-04-15 21:20:48 +0900

    google Login

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.composetest
 
-import android.app.Activity
 import android.content.ContentValues.TAG
-import android.content.Intent
-import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
@@ -61,15 +60,16 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.example.composetest.ui.theme.ComposeTestTheme
 import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.GoogleApiClient
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -78,97 +78,30 @@ import com.skydoves.landscapist.glide.GlideImage
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint //의존성주입
-class MainActivity : Activity() {
-
-    // [START declare_auth]
-    private lateinit var auth: FirebaseAuth
-    private lateinit var googleApiClient : GoogleApiClient
-    // [END declare_auth]
-
-    private lateinit var googleSignInClient: GoogleSignInClient
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
 
-        // [START config_signin]
-        // Configure Google Sign In
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
+            val navController = rememberNavController()
 
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-        // [END config_signin]
+            NavHost(navController = navController, startDestination = "screen1") {
+                composable("screen1") {
+                    GreetingPreview(navController)
+                }
+                composable("screen2") {
+                    Page1(navController)
 
-        // [START initialize_auth]
-        // Initialize Firebase Auth
-        auth = Firebase.auth
-        // [END initialize_auth]
-    }
-
-    // [START on_start_check_user]
-    override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        updateUI(currentUser)
-    }
-    // [END on_start_check_user]
-
-    // [START onactivityresult]
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
+                }
+                composable("screen3") {
+                }
             }
         }
     }
-    // [END onactivityresult]
-
-    // [START auth_with_google]
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                    val user = auth.currentUser
-                    updateUI(user)
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                    updateUI(null)
-                }
-            }
-    }
-    // [END auth_with_google]
-
-    // [START signin]
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-    // [END signin]
-
-    private fun updateUI(user: FirebaseUser?) {
-    }
-
-    companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
-    }
 }
+
+
 
 @Preview
 @Composable
@@ -313,15 +246,10 @@ fun BottomNavigationBar(modifier: Modifier=Modifier
 }
 
 @Composable
-fun GoogleLoginbtn(){
+fun GoogleLoginbtn(navController: NavController){
     val context = LocalContext.current
     val token = stringResource(R.string.default_web_client_id)
 
-//    val launcherNav = rememberLauncherForActivityResult(
-//        contract = ActivityResultContracts.StartActivityForResult()
-//    ) {
-//        navController.navigate(Screen.MainScreen.route)
-//    }
     val auth : FirebaseAuth = Firebase.auth
     val launcher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
@@ -334,11 +262,14 @@ fun GoogleLoginbtn(){
                 FirebaseAuth.getInstance().signInWithCredential(credential)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
+                            navController.navigate("screen2")
                             //이쪽코드를 통해 네비게이션 활용
                             Log.d("TAG", "Google Sign In Success")
                             Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                            Log.d(TAG, "firebaseAuthWithGoogle:" + account.idToken) //이 토큰을 주로 식별용으로 사용할것.
                             Log.d(TAG, "firebaseAuthWithGoogle:" + auth.currentUser.toString())
-
+                            task.result.user
+                            task.result.credential
 
                         }
                     }
@@ -365,17 +296,17 @@ fun GoogleLoginbtn(){
     }
 }
 
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    name = "DefaultPreviewDark"
-)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    name = "DefaultPreviewLight"
-)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_YES,
+//    name = "DefaultPreviewDark"
+//)
+//@Preview(
+//    uiMode = Configuration.UI_MODE_NIGHT_NO,
+//    name = "DefaultPreviewLight"
+//)
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview(navController: NavController) {
     ComposeTestTheme {
         val list1: List<Item> = listOf(
             Item("kim", 1, R.drawable.ic_launcher_foreground),
@@ -433,7 +364,7 @@ fun GreetingPreview() {
                         .fillMaxSize()
                         .padding(it)
                 ) {
-                    GoogleLoginbtn()
+                    GoogleLoginbtn(navController)
                     Spacer(modifier = Modifier.padding(0.dp,0.dp,0.dp,3.5.dp))
                     LazyColumn {
                         items(list1) { item ->
