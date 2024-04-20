6468bdacae06a1d28e329d616b413f65748b72ed
kkh <kkhyungyung0@naver.com>
2024-04-20 21:41:50 +0900

    commit

--- app/src/main/java/com/example/composetest/MainActivity.kt
+++ app/src/main/java/com/example/composetest/MainActivity.kt
@@ -80,7 +80,6 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
